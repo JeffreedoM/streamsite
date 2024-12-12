18210ea9156c71cfe778ec78d8e9241226c0ade4
Jeffrey Nunez <jeffreynunez029@gmail.com>
2024-12-13 07:35:12 +0800

    update enrollment

--- resources/js/Pages/Courses/Student/Index.jsx
+++ resources/js/Pages/Courses/Student/Index.jsx
@@ -44,7 +44,6 @@ const Index = ({ enrolledCourses, unenrolledCourses }) => {
     router.post(`courses/${id}`, { password });
   }, []);
 
-<<<<<<< HEAD
   return (
     <Tabs defaultValue="enrolled">
       <TabsList>
@@ -130,32 +129,6 @@ const Index = ({ enrolledCourses, unenrolledCourses }) => {
         ))}
       </TabsContent>
     </Tabs>
-=======
-const Index = ({ courses}) => {
-  console.log("courses:", courses);
-  return (
-    <div className="grid grid-cols-2 gap-x-5 gap-y-10 lg:grid-cols-3 xl:grid-cols-4">
-      {courses.map((course) => (
-        <Link
-          // href={`courses/${course.id}`}
-          href={`courses/enroll/${course.id}`}
-          className="group flex max-h-[280px] cursor-pointer flex-col gap-y-2 overflow-hidden bg-background"
-        >
-          <img
-            src={course.course_image ? course.course_image : img}
-            alt=""
-            className="h-[150px] origin-bottom overflow-hidden object-cover transition-transform duration-150 group-hover:scale-110 md:h-[200px]"
-          />
-          <div className="flex flex-grow flex-col justify-between space-y-2">
-            <h3 className="line-clamp-2 text-sm font-semibold">
-              {course.course_title}
-            </h3>
-            {/* <p className="text-xs">3 Chapters</p> */}
-          </div>
-        </Link>
-      ))}
-    </div>
->>>>>>> d7d4eadc66f1978d5bf88ba69136f0d03c026f90
   );
 };
 
